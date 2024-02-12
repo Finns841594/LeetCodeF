cc8192c031a5b61e955f1407392abc73339f1707
Feng Y <65496134+Finns841594@users.noreply.github.com>
2024-02-12 11:18:53 +0100

    Time: 59 ms (49.81%) | Memory: 51.4 MB (23.28%) - LeetSync

--- 66-plus-one/plus-one.ts
+++ 66-plus-one/plus-one.ts
@@ -1,16 +1,12 @@
-function plusOneByIndex(digits: number[],index:number): number[]{
-    if (digits[index] < 9){
-        digits[index] = digits[index] + 1
-        return digits
-    } else if (index === 0) {
-        digits[index] = 0
-        digits.unshift(1)
-        return digits
-    }
-    digits[index] = 0
-    return plusOneByIndex(digits, index-1)
-}
-
 function plusOne(digits: number[]): number[] {
-    return plusOneByIndex(digits,digits.length-1)
+    let lastIndex = digits.length - 1
+    for (let i = lastIndex; i >= 0; i--){
+        if (digits[i]<9){
+            digits[i]++
+            return digits
+        }
+        digits[i]=0
+    }
+    digits.unshift(1)
+    return digits
 };
