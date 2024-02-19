50b0a11c4971dfca846d57b706f3581057598719
Feng Y <65496134+Finns841594@users.noreply.github.com>
2024-02-19 11:43:28 +0100

    Time: 125 ms (36.05%) | Memory: 55.9 MB (32.85%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.ts
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -1,15 +1,32 @@
-const dictionSymbol = ['I','IV','V','IX','X','XL','L','XC','C','CD','D','CM','M']
-const dictionValue = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+const diction = {
+    'I': 1,
+    'IV': 4,
+    'V': 5,
+    'IX': 9,
+    'X': 10,
+    'XL': 40,
+    'L': 50,
+    'XC': 90,
+    'C': 100,
+    'CD': 400,
+    'D': 500,
+    'CM': 900,
+    'M': 1000
+}
+
 
 function romanToInt(s: string): number {
     let sum = 0
     for (let i = 0; i<s.length; i++){
         let j = i + 1
-        if(dictionSymbol.indexOf(s[i]) >= dictionSymbol.indexOf(s[j])){
-            sum += dictionValue[dictionSymbol.indexOf(s[i])]
+        if (i < s.length-1){
+            j = i + 1
+        }
+        if(diction[s[i]] >= diction[s[j]] || i === s.length-1){
+            sum += diction[s[i]]
             // console.log('regular plus', sum)
         } else {
-            sum += dictionValue[dictionSymbol.indexOf(s[i]+s[j])]
+            sum += diction[s[i]+s[j]]
             i++
             // console.log('minus one plus', sum)
         }
