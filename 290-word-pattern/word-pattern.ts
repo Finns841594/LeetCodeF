cf30bc835f13021b2b098fe61c35c3e9f4109e77
Feng Y <65496134+Finns841594@users.noreply.github.com>
2024-02-22 10:34:09 +0100

    Time: 60 ms (32.34%) | Memory: 50 MB (48.51%) - LeetSync

--- 290-word-pattern/word-pattern.ts
+++ 290-word-pattern/word-pattern.ts
@@ -1,24 +1,20 @@
 function wordPattern(pattern: string, s: string): boolean {
     let pMap = new Map()
     let sArray = s.split(' ')
-    let pArray = pattern.split('')
-    if (sArray.length != pArray.length){
-        return false
-    }
-
-    let sSet = new Set(sArray)
-    let pSet= new Set(pArray)
-    if (sSet.size != pSet.size) {
+    if (sArray.length != pattern.length){
         return false
     }
     
     for (let i = 0; i < pattern.length; i++){
-        if (!pMap.has(pattern[i])){
-            pMap.set(pattern[i],sArray[i])
-        } else {
+        if (pMap.has(pattern[i])){
             if (pMap.get(pattern[i]) != sArray[i]){
                 return false
             }
+        } else {
+            if (Array.from(pMap.values()).includes(sArray[i])){
+                return false
+            }
+            pMap.set(pattern[i],sArray[i])
         }
     }
     return true
