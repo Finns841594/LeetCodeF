b39d06cac72eede46ab975d456e93e5f54da9c35
Feng Y <65496134+Finns841594@users.noreply.github.com>
2024-02-27 10:43:23 +0100

    Time: 64 ms (69.12%) | Memory: 53.5 MB (6.54%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -2,7 +2,7 @@ function twoSum(nums: number[], target: number): number[] {
     const tempMap = new Map()
     for (let i = 0; i < nums.length; i++) {
         if (tempMap.has(target - nums[i])) {
-            return [tempMap.get(target - nums[i]), i]
+            return [i, tempMap.get(target - nums[i])]
         }
         tempMap.set(nums[i],i)
     }
