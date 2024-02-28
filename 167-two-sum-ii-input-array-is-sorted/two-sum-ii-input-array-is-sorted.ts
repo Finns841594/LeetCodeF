4adbfdcdc9fa309ca43e24ab83e70b666a981ae9
Feng Y <65496134+Finns841594@users.noreply.github.com>
2024-02-28 11:07:46 +0100

    Time: 71 ms (29.13%) | Memory: 51.1 MB (61.78%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -2,12 +2,13 @@ function twoSum(numbers: number[], target: number): number[] {
     let small = 0
     let big = numbers.length - 1
     while (small < big) {
-        if (numbers[small] + numbers[big] > target) {
-            big--
-        } else if (numbers[small] + numbers[big] < target) {
+        let sum = numbers[small] + numbers[big]
+        if (sum === target) {
+            return [small+1,big+1]
+        } else if (sum < target) {
             small++
         } else {
-            return [small+1,big+1]
+            big--
         }
     }
 };
