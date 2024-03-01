a4e132587771efc483f936ac5f80ccdbcc9aa35d
Feng Y <65496134+Finns841594@users.noreply.github.com>
2024-03-01 10:22:47 +0100

    Time: 99 ms (59.71%) | Memory: 60.8 MB (27.71%) - LeetSync

--- 56-merge-intervals/merge-intervals.ts
+++ 56-merge-intervals/merge-intervals.ts
@@ -1,24 +1,21 @@
-function mergeTwo(intervalOne: number[], intervalTwo: number[]):number[] {
-    if (intervalOne[1] < intervalTwo[0]) {
-        return undefined // not merging
-    } else if (intervalOne[1] > intervalTwo[1]) {
-        return intervalOne
-    } else {
-        return [intervalOne[0],intervalTwo[1]]
-    }
-}
-
 function merge(intervals: number[][]): number[][] {
     const sortedIntervals = intervals.sort((a,b) => a[0]-b[0])
-    let result = [sortedIntervals[0]]
-    for (let i = 1; i < sortedIntervals.length; i++) {
-        const mergedByTwo = mergeTwo(result[result.length - 1], sortedIntervals[i])
-        if (mergedByTwo) {
-            result[result.length - 1] = mergedByTwo
+    let result = []
+    let start = sortedIntervals[0][0]
+    let end = sortedIntervals[0][1]
+    for (let interval of sortedIntervals) {
+        if (interval[0] <= end) {
+            // in the case of merging
+            end = Math.max(interval[1], end)
         } else {
-            result.push(sortedIntervals[i])
+            // in the case of not merging, move on to next start-end pair
+            result.push([start, end])
+            start = interval[0]
+            end = interval[1]
         }
-
     }
+    // put in the last pair
+    result.push([start,end])
+    
     return result
 };
