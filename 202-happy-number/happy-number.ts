42eea1ed81e79687a097e6d4b73fca49b06fb674
Feng Y <65496134+Finns841594@users.noreply.github.com>
2024-03-05 11:08:23 +0100

    Time: 70 ms (28.59%) | Memory: 52.4 MB (19.63%) - LeetSync

--- 202-happy-number/happy-number.ts
+++ 202-happy-number/happy-number.ts
@@ -1,11 +1,14 @@
-function isHappy(n: number): boolean {
-    if (n === 1 || n === 7) {
-        return true
-    } else if (n>1 && n<10) {
-        return false
-    }
+function isHappyMemo(n:number, sums:number[]):boolean {
+    if (sums[sums.length-1] === 1) return true
+    if (n in sums) return false
+
     let sum = 0
     const digitArray = n.toString().split('')
     digitArray.forEach(item => sum = sum + Number(item)*Number(item))
-    return isHappy(sum)
+    sums.push(sum)
+    return isHappyMemo(sum, sums)
+}
+
+function isHappy(n: number): boolean {
+    return isHappyMemo(n,[])
 };
