f24be25095a2cb1fc665677c4282e0c3cf8ab7b7
Feng Y <65496134+Finns841594@users.noreply.github.com>
2024-03-06 15:33:37 +0100

    Time: 65 ms (40.35%) | Memory: 51.9 MB (69.91%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.ts
+++ 20-valid-parentheses/valid-parentheses.ts
@@ -6,36 +6,16 @@ function isPair(a:string,b:string): boolean {
     }
 }
 
-function pairReplace(startIndex:number,arrayOfParentheses:string[]) {
-    let step = 0
-    while(arrayOfParentheses[startIndex-step] != '0') {
-        if (isPair(arrayOfParentheses[startIndex-step],arrayOfParentheses[startIndex+1+step])) {
-            arrayOfParentheses[startIndex-step] = '0'
-            arrayOfParentheses[startIndex+1+step] = '0'
-            if (startIndex-step>0) {
-                step++
-            } else {
-                break
-            }
-        } else {
-            break
-        }
-    }
-}
-
 function isValid(s: string): boolean {
     const array = s.split('')
-    if (array.length === 0) {
-        return true
-    }
-    for (let i = 0; i < array.length; i++) {
-        console.log(i)
-        if (isPair(array[i],array[i+1])) {
-            pairReplace(i,array)
+    const memo = [array[0]]
+    for (let i = 1; i < array.length; i++) {
+        if (isPair(memo[memo.length-1],array[i])) {
+            memo.pop()
+        } else {
+            memo.push(array[i])
         }
     }
 
-    const result = array.filter(item => item != '0')
-    if (result.length === array.length) return false
-    return isValid(result.join(''))
+    return memo.length === 0
 };
